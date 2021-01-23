7805c800e6ac3ddc02e6b029c2b47e4aa6630357
kaanberke <kaanberkeugurlar@gmail.com>
2021-01-23 19:49:40 +0300

    User class was created.

---
+++ app/src/main/java/com/project/cryptocurrencychat/User.java
@@ -0,0 +1,46 @@
+package com.project.cryptocurrencychat;
+
+public class User {
+    public String name, age, email;
+
+    public User(){}
+
+    public User(String name, String age, String email){
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age='" + age + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
